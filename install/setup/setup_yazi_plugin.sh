fee1bc6e3232fc7949dfba2e9fe6b7ed05529fac
Vladimir Galović <vgalovic99@gmail.com>
2024-12-14 12:28:27 +0100

    Improved setup_yazi_plugins.sh
    - Added code that remove old package.toml, theme.toml and plugins/
    - Created download_catppuccin_theme() to download latest catppuccin
    theme for yazi from catppuccin repository

--- install/setup/setup_yazi_plugin.sh
+++ install/setup/setup_yazi_plugin.sh
@@ -2,6 +2,24 @@
 
 source "$HOME/.dotfiles/install/setup/print_and_log.sh"
 
+yazi_dir="$HOME/.config/yazi"
+plugin_dir="$yazi_dir/plugins"
+
+if [ -f "$HOME/.config/yazi/package.toml" ]; then
+    print_msg "pace.toml already exists. Removing old file..."
+    rm "$HOME/.config/yazi/package.toml" || print_error "Failed to remove old pace.toml file."
+fi
+
+if [ -f "$HOME/.config/yazi/theme.toml" ]; then
+    print_msg "theme.toml already exists. Removing old file..."
+    rm "$HOME/.config/yazi/theme.toml" || print_error "Failed to remove old theme.toml file."
+fi
+
+if [ -d "$HOME/.config/yazi/plugins" ]; then
+    print_msg "plugins directory already exists. Removing old directory..."
+    rm -rf "$HOME/.config/yazi/plugins" || print_error "Failed to remove old plugins directory."
+fi
+
 # Function to check and install APT packages required for Yazi
 install_yazi_apt_required() {
     local apt_yazi_required_apps=(
@@ -27,26 +45,24 @@ install_yazi_apt_required() {
 
 # Function to install Yazi plugins
 install_yazi_plugins() {
-    local plugin_dir="$HOME/.config/yazi/plugins/"
-
     # Create the plugin directory if it doesn't exist
-    mkdir -p "$plugin_dir"
+    mkdir -p "$plugin_dir" || { print_error "Failed to create $plugin_dir directory."; return 1; }
 
     # List of Yazi plugins to install
     declare -a plugins=(
-        "https://github.com/sharklasers996/eza-preview.yazi.git" # eza-preview.yazi
+        "https://github.com/sharklasers996/eza-preview.yazi.git" # [[ eza-preview.yazi ]]
     )
 
     # List of Ya plugins to install
     declare -a ya_plugins=(
-        "yazi-rs/plugins:chmod" # chmod.yazi
-        "Sonico98/exifaudio" # exifaudio.yazi
-        "yazi-rs/plugins:full-border" # full-border.yazi
-        "yazi-rs/plugins:git" # git.yazi
-        "Reledia/hexyl" # hexyl.yazi
-        "Lil-Dank/lazygit" # lazygit.yazi
-        "AnirudhG07/rich-preview" # rich-preview.yazi
-        "Rolv-Apneseth/starship" # starship.yazi
+        "yazi-rs/plugins:chmod" # [[ chmod.yazi ]]
+        "Sonico98/exifaudio" # [[ exifaudio.yazi ]]
+        "yazi-rs/plugins:full-border" # [[ full-border.yazi ]]
+        "yazi-rs/plugins:git" # [[ git.yazi ]]
+        "Reledia/hexyl" # [[ hexyl.yazi ]]
+        "Lil-Dank/lazygit" # [[ lazygit.yazi ]]
+        "AnirudhG07/rich-preview" # [[ rich-preview.yazi ]]
+        "Rolv-Apneseth/starship" # [[ starship.yazi ]]
     )
 
     # Install each GitHub plugin
@@ -67,18 +83,25 @@ install_yazi_plugins() {
     # Check if 'ya' command is available
     if ! command -v ya &> /dev/null; then
         print_error "'ya' command is not available. Skipping Ya plugin installation."
-        return 1
+    else
+        # Install each Ya plugin
+        for ya_plugin in "${ya_plugins[@]}"; do
+            print_msg "Installing ya plugin: $ya_plugin..."
+            if ! ya pack -a "$ya_plugin"; then
+                print_error "Failed to install ya plugin: $ya_plugin."
+            else
+                print_msg "$ya_plugin installed successfully."
+            fi
+        done
     fi
+}
 
-    # Install each Ya plugin
-    for ya_plugin in "${ya_plugins[@]}"; do
-        print_msg "Installing ya plugin: $ya_plugin..."
-        if ! ya pack -a "$ya_plugin"; then
-            print_error "Failed to install ya plugin: $ya_plugin."
-        else
-            print_msg "$ya_plugin installed successfully."
-        fi
-    done
+# Yazi cattpuccin theme
+download_catppuccin_theme() {
+    print_msg "Downloading catppuccin-mocha-blue.toml theme..."
+    cd "$yazi_dir" || print_error "Failed to change to $yazi_dir directory."
+    wget https://raw.githubusercontent.com/catppuccin/yazi/refs/heads/main/themes/mocha/catppuccin-mocha-blue.toml || print_error "Failed to download catppuccin-mocha-blue.toml theme."
+    mv catppuccin-mocha-blue.toml theme.toml || print_error "Failed to move catppuccin-mocha-mauve.toml theme."
 }
 
 # If Yazi APT packages are needed
@@ -86,6 +109,9 @@ install_yazi_apt_required
 
 # Main script execution
 install_yazi_plugins
+
+# Install Yazi catppuccin theme
+download_catppuccin_theme
 
 print_msg "Yazi plugins installation complete!"
 
