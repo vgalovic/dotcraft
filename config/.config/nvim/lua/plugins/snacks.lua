5bdfa2e498311c688046d5a3366fc1b904e47dcc
Vladimir Galović <vgalovic99@gmail.com>
2024-12-14 14:03:42 +0100

    Added snacks.nvim to config
    - Removed dashboard.lua as deshboard.nvim is replaced with dashbord
    plugin in snacks.nvim
    - Removed blankline.lua as indent-blankline.nvim is replaced with indent
    plugin in snacks.nvim
    - In noice.lua removed notify.nvim as it is replaced with notifier
    plugin in snacks.nvim

---
+++ config/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		requires = {
+			{ "nvim-tree/nvim-web-devicons" },
+		},
+		opts = {
+			dashboard = {
+				example = "advanced",
+			},
+			bigfile = { enabled = true },
+			indent = { enabled = true },
+			scroll = { enabled = true },
+			notifier = {
+				enabled = true,
+				style = "fancy", -- "compact" "fancy" "minimal"
+				top_down = true,
+
+				lsp_utils = require("config/autocommand").setup_lsp_progress(),
+			},
+			quickfile = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = {
+				enabled = true,
+				notify_jump = true,
+			},
+		},
+	},
+}
