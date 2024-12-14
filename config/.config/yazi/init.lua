4a6d22086415731ce5949e6d5c208779ad0af90c
Vladimir Galović <vgalovic99@gmail.com>
2024-12-14 23:12:13 +0100

    Updated eza-preview setup

--- config/.config/yazi/init.lua
+++ config/.config/yazi/init.lua
@@ -1,7 +1,14 @@
 require("starship"):setup({ config_file = "$HOME/.config/yazi/starship_yazi.toml" })
-require("eza-preview"):setup()
 require("git"):setup()
 require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
+})
+require("eza-preview"):setup({
+	-- Determines the directory depth level to tree preview
+	level = 3,
+	-- Whether to follow symlinks when previewing directories
+	follow_symlinks = true,
+	-- Whether to show target file info instead of symlink info
+	dereference = true,
 })
