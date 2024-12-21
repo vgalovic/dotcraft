369df3ebff47e4e8a93a0d0eae26f988882a63ae
Vladimir Galović <vgalovic99@gmail.com>
2024-12-21 23:16:35 +0100

    Improved setup_games.sh and added a game

--- install/setup/setup_games.sh
+++ install/setup/setup_games.sh
@@ -33,25 +33,40 @@ install_minecraft() {
 }
 
 install_games() {
-    print_msg "Installing Steam..."
-    sudo apt install -y steam || print_error "Failed to install Steam."
-    print_msg "Steam installed."
+    if prompt_yes_default "Do you want to install Steam?"; then
+        print_msg "Installing Steam..."
+        sudo apt install -y steam || print_error "Failed to install Steam."
+        print_msg "Steam installed."
+    else
+        print_msg "Skipping Steam installation."
+    fi
 
     # Check for Flatpak and install Heroic Games Launcher if available
     if command -v flatpak &> /dev/null; then
-        print_msg "Installing Heroic Launcher..."
-        flatpak install -y flathub com.heroicgameslauncher.hgl || print_error "Failed to install Heroic Games Launcher."
-        print_msg "Heroic Launcher installed."
-    else
-        print_msg "Flatpak is not installed. Skipping Heroic Launcher installation."
+        if prompt_yes_default "Do you want to install Heroic Launcher?"; then
+            print_msg "Installing Heroic Launcher..."
+            flatpak install -y flathub com.heroicgameslauncher.hgl || print_error "Failed to install Heroic Games Launcher."
+            print_msg "Heroic Launcher installed."
+        else
+            print_msg "Skipping Heroic Launcher installation."
+        fi
+
+        if prompt_yes_defualt "Do you wnat to install Space Cadet Pinball?"; then
+            print_msg "Installing Space Cadet Pinball..."
+            flatpak install -y flathub com.github.k4zmu2a.spacecadetpinball || print_error "Failed to install Space Cadet Pinball."
+            print_msg "Space Cadet Pinball installed."
+        else
+            print_msg "Skipping Space Cadet Pinball installation."
+        fi
     fi
 
     # Check if Minecraft is already installed; if not, install it
-    if ! command -v minecraft-launcher &> /dev/null; then    
+    if prompt_yes_default "Do you want to install Minecraft?"; then
         install_minecraft
     else
-        print_msg "Minecraft already installed."
+        print_msg "Skipping Minecraft installation."
     fi
+
 }
 
 install_games
