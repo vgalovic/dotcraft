6006838319b1f56e9f641a8bedbe8b5bfa2871a6
Vladimir Galović <vgalovic99@gmail.com>
2025-01-07 23:04:49 +0100

    Changeed cmdline from "cmdline_popup" to "cmdline" in noce.nvim

--- config/.config/nvim/lua/plugins/noice.lua
+++ config/.config/nvim/lua/plugins/noice.lua
@@ -1,49 +1,55 @@
 return {
-	{
-		"folke/noice.nvim",
-		lazy = false, -- Load this plugin immediately
-		priority = 1000, -- High priority to ensure it loads first
-		requires = {
-			{ "MunifTanjim/nui.nvim" }, -- UI component library required by noice.nvim
-		},
-		config = function()
-			require("noice").setup({
-				notify = {
-					enabled = false,
-				},
-				lsp = {
-					signature = {
-						enabled = true,
-						auto_open = { enabled = true },
-					},
-					hover = {
-						enabled = true,
-						silent = true,
-						border = "rounded",
-					},
-					progress = {
-						enabled = false, -- Disable LSP progress notifications
-					},
-				},
-				presets = {
-					bottom_search = true, -- Use a classic bottom cmdline for search
-					command_palette = true, -- Position the cmdline and popupmenu together
-					long_message_to_split = true, -- Long messages will be sent to a split
-					lsp_doc_border = true, -- Add a border to hover docs and signature help
-				},
-				command = {
-					enable = true,
-				},
-				routes = {
-					{
-						filter = {
-							event = "lsp",
-							kind = "progress",
-						},
-						opts = { skip = true }, -- Filter out LSP progress events
-					},
-				},
-			})
-		end,
+	"folke/noice.nvim",
+	lazy = false, -- Load this plugin immediately
+	priority = 1000, -- High priority to ensure it loads first
+	requires = {
+		{ "MunifTanjim/nui.nvim" }, -- UI component library required by noice.nvim
 	},
+	config = function()
+		require("noice").setup({
+			cmdline = {
+				enabled = true,
+				view = "cmdline", -- cmdline or cmdline_popup
+			},
+			notify = {
+				enabled = false,
+			},
+			lsp = {
+				signature = {
+					enabled = true,
+					auto_open = { enabled = true },
+				},
+				hover = {
+					enabled = true,
+					silent = true,
+					border = "rounded",
+				},
+				progress = {
+					enabled = false, -- Disable LSP progress notifications
+				},
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true, -- Use a classic bottom cmdline for search
+				command_palette = true, -- Position the cmdline and popupmenu together
+				long_message_to_split = true, -- Long messages will be sent to a split
+				lsp_doc_border = true, -- Add a border to hover docs and signature help
+			},
+			command = {
+				enable = true,
+			},
+			routes = {
+				{
+					filter = {
+						event = "lsp",
+						kind = "progress",
+					},
+					opts = { skip = true }, -- Filter out LSP progress events
+				},
+			},
+		})
+	end,
 }
