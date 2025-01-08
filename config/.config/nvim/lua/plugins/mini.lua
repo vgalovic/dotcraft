a20487726d52304881141c47c492cf4f9671adde
Vladimir Galović <vgalovic99@gmail.com>
2025-01-08 21:37:03 +0100

    Moved from gitsigns.nvim to Mini.Diff

--- config/.config/nvim/lua/plugins/mini.lua
+++ config/.config/nvim/lua/plugins/mini.lua
@@ -11,23 +11,13 @@ return { -- Collection of various small independent plugins/modules
 		--  - ci'  - [C]hange [I]nside [']quote
 		require("mini.ai").setup({ n_lines = 500 })
 		--
-		-- Better indentation
-		--
-		-- vii Object scope
-		-- vai Object scope with border
-		-- [i Go to top of scope
-		-- ]i Go to bottom of scope
-		require("mini.indentscope").setup({
-			symbol = "│",
-		})
-		--
 		-- Add/delete/replace surroundings (brackets, quotes, etc.)
 		--
 		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 		-- - sd'   - [S]urround [D]elete [']quotes
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
-
+		--
 		require("mini.jump2d").setup({
 			hooks = {
 				pre = function()
@@ -43,6 +33,15 @@ return { -- Collection of various small independent plugins/modules
 						vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" })
 					end,
 				},
+			},
+		})
+		--
+		require("mini.diff").setup({
+			view = {
+				style = "sign", -- number | sign
+
+				-- signs = { add = "+", change = "~", delete = "-" },
+				signs = { add = "▕▏", change = "▕▏", delete = "▁▁" },
 			},
 		})
 		--
