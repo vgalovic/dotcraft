fc24c058eee9c63e80e8399acf82b50c3c93a6be
Vladimir Galović <vgalovic99@gmail.com>
2025-01-08 21:38:00 +0100

    Added scope and statuscolumn for list of used plugins in Snack.nvim

--- config/.config/nvim/lua/plugins/snacks.lua
+++ config/.config/nvim/lua/plugins/snacks.lua
@@ -1,36 +1,42 @@
 return {
-	{
-		"folke/snacks.nvim",
-		priority = 1000,
-		lazy = false,
-		requires = {
-			{ "nvim-tree/nvim-web-devicons" },
-		},
-		opts = {
-			dashboard = {
-				example = "advanced",
-			},
-			bigfile = { enabled = true },
-			indent = { enabled = true },
-			scroll = { enabled = true },
-			scope = { enabled = false },
-			input = { enabled = false },
-			notifier = {
-				enabled = true,
-				style = "compact", -- "compact" "fancy" "minimal"
-				top_down = false,
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	requires = {
+		{ "nvim-tree/nvim-web-devicons" },
+	},
+	opts = {
+		dashboard = { example = "advanced" },
+		bigfile = { enabled = true },
+		indent = { enabled = true },
+		scroll = { enabled = true },
+		scope = { enabled = true },
+		input = { enabled = false },
+		notifier = {
+			enabled = true,
+			style = "compact", -- "compact" "fancy" "minimal"
+			top_down = false,
 
-				lsp_utils = require("config/autocommands").setup_lsp_progress(),
+			lsp_utils = require("config/autocommands").setup_lsp_progress(),
+		},
+		lazygit = { configure = false },
+		quickfile = { enabled = true },
+		statuscolumn = {
+			enabled = true,
+			left = { "mark", "sign" },
+			right = { "fold", "git" },
+			folds = {
+				open = false,
+				git_hl = true,
 			},
-			lazygit = {
-				configure = false,
+			git = {
+				patterns = { "MiniDiffSign" },
 			},
-			quickfile = { enabled = true },
-			statuscolumn = { enabled = true },
-			words = {
-				enabled = true,
-				notify_jump = true,
-			},
+			refresh = 50,
+		},
+		words = {
+			enabled = true,
+			notify_jump = true,
 		},
 	},
 }
