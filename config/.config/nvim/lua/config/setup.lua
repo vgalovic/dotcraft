3dbf5a74cf986cb81d1a8e4555dc9c7ce46301aa
Vladimir Galović <vgalovic99@gmail.com>
2025-01-08 22:38:21 +0100

    Disabled showmode in neovim

--- config/.config/nvim/lua/config/setup.lua
+++ config/.config/nvim/lua/config/setup.lua
@@ -35,7 +35,7 @@ vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
-vim.opt.showmode = true
+vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
