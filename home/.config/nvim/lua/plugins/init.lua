9906453752bcb3367c3a9b688251ffc2739c36f1
Vladimir Galović <vgalovic99@gmail.com>
2025-01-23 21:52:59 +0100

    Organized plugins into groups for better modularity

---
+++ home/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+	{ import = "plugins.ai" },
+	{ import = "plugins.coding" },
+	{ import = "plugins.colorscheme" },
+	{ import = "plugins.completion" },
+	{ import = "plugins.core" },
+	{ import = "plugins.lsp" },
+	{ import = "plugins.misc" },
+	{ import = "plugins.ui" },
+}
