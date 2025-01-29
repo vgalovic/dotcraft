345a0b662f2b6bcc3fedd911455831ed89867b29
Vladimir Galović <vgalovic99@gmail.com>
2025-01-29 13:49:50 +0100

    Renamed save_as.lua to buffer.lua and added functions for managing files and buffers.

--- home/.config/nvim/lua/utils/mapping_actions/buffer.lua
+++ home/.config/nvim/lua/utils/mapping_actions/buffer.lua
@@ -1,10 +1,10 @@
 ---@diagnostic disable: param-type-mismatch
----@class save_as
+---@class buffer
 
 local M = {}
 
 -- If it is a new file, prompt for a filename and save it, otherwise, save the current file
-M.SaveAs = function()
+local SaveAs = function()
 	local current_file = vim.fn.expand("%:p") -- Get the current file path
 	if current_file ~= "" then
 		vim.cmd("write") -- If a file already exists, just save it
@@ -40,6 +40,36 @@ M.SaveAs = function()
 				end
 			end
 		end)
+	end
+end
+
+-- Save the current buffer
+M.save = function()
+	-- Check if the current mode is insert mode
+	local mode = vim.api.nvim_get_mode().mode
+	if mode == "i" then
+		-- Save and return to insert mode
+		SaveAs()
+		vim.cmd("startinsert")
+	else
+		-- Just save
+		SaveAs()
+	end
+end
+
+-- Save on open empty buffer
+M.new_file = function()
+	vim.cmd("enew")
+	SaveAs()
+end
+
+-- Delete all buffers except the current one
+M.delete_other_buffers = function()
+	local current_buf = vim.api.nvim_get_current_buf() -- Get the current buffer number
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) then
+			vim.api.nvim_buf_delete(buf, { force = true }) -- Delete the buffer
+		end
 	end
 end
 
