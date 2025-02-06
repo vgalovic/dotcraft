56d7e1e59ab86a228ccb05fd3a888ff4e46fbd7e
Vladimir Galović <vgalovic99@gmail.com>
2025-02-06 21:46:03 +0100

    Removed diagnostics form lua_ls config

--- home/.config/nvim/lua/lsp/lspconfig/lua_ls.lua
+++ home/.config/nvim/lua/lsp/lspconfig/lua_ls.lua
@@ -8,7 +8,6 @@ return function()
 				completion = { callSnippet = "Replace" }, -- Replace function snippets on call
 				workspace = { checkThirdParty = false }, -- Disable third-party library checks
 				telemetry = { enable = false }, -- Disable telemetry data collection
-				diagnostics = { disable = { "missing-fields" } }, -- Disable "missing-fields" warnings
 			},
 		},
 	})
