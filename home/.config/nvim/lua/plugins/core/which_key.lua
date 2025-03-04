19522b6b29a0d9291d67383cda262f015ab9e392
Vladimir Galović <vgalovic99@gmail.com>
2025-03-04 16:53:46 +0100

    Improved which-key.nvim

--- home/.config/nvim/lua/plugins/core/which_key.lua
+++ home/.config/nvim/lua/plugins/core/which_key.lua
@@ -1,25 +1,32 @@
 return {
 	"folke/which-key.nvim",
-	event = "VimEnter",
+	event = "VimEnter", -- Ensures the plugin loads when Vim starts
 	opts = {
 		--- @default "classic"
 		--- @options ["classic", "modern", "helix"]
-		preset = "helix",
-
-		delay = 0,
-
+		preset = "helix", -- Optional style preset for which-key
+		delay = 0, -- No delay in showing the hints
 		icons = {
-			mappings = false,
+			-- mappings = vim.g.nerdfonts,
+			mappings = false, -- Disable icons for mappings
 		},
-
 		spec = {
 			{ "<leader>d", group = "Find and Delete", mode = { "n", "v" } },
 			{ "<leader>c", group = "Code" },
 			{ "<leader>g", group = "Git", mode = { "n" } },
 			{ "<leader>r", group = "Find and Replace", mode = { "n", "v" } },
-			{ "<leader>p", group = "Packge manager", mode = { "n" } },
+			{ "<leader>p", group = "Package Manager", mode = { "n" } },
 			{ "<leader>s", group = "Search", mode = { "n", "v" } },
 			{ "<leader>t", group = "Toggle", mode = { "n" } },
+		},
+	},
+	keys = {
+		{
+			"<c-w><space>",
+			function()
+				require("which-key").show({ keys = "<c-w>", loop = true })
+			end,
+			desc = "Window Hydra Mode",
 		},
 	},
 }
