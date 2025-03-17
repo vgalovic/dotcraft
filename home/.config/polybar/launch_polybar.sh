781b463cc3d556d1915f631d177217aea8b620fa
Vladimir Galović <vgalovic99@gmail.com>
2025-03-17 14:55:28 +0100

    Improved polybar config

--- home/.config/polybar/launch_polybar.sh
+++ home/.config/polybar/launch_polybar.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Launch Polybar
+# Kill existing Polybar instances
+killall -q polybar
+
+# Launch Polybar for each monitor
 if command -v xrandr > /dev/null; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar --reload toph &
