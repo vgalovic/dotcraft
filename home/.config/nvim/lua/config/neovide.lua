bc841a847003ef230e43f7750f5e264b9d8b56cb
Vladimir Galović <vgalovic99@gmail.com>
2025-04-08 15:34:48 +0200

    Added neovide config in neovim

---
+++ home/.config/nvim/lua/config/neovide.lua
@@ -0,0 +1,12 @@
+vim.o.guifont = "JetBrainsMono Nerd Font:h11"
+
+vim.g.neovide_floating_shadow = true
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 45
+vim.g.neovide_light_radius = 5
+
+vim.g.neovide_theme = "auto"
+
+vim.g.neovide_remember_window_size = true
+
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
