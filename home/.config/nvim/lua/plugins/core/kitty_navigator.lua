70a3e7d305423b49b9a98cd22c7432f50c817ac1
Vladimir Galović <vgalovic99@gmail.com>
2025-04-21 13:01:21 +0200

    Improved keys for vim-kitty-navigator

--- home/.config/nvim/lua/plugins/core/kitty_navigator.lua
+++ home/.config/nvim/lua/plugins/core/kitty_navigator.lua
@@ -1,16 +1,16 @@
 return {
-  "knubie/vim-kitty-navigator",
-  keys = {
-    { "<C-h>", "<cmd>KittyNavigateLeft<cr>",  desc = "Move to panel Left" },
-    { "<C-j>", "<cmd>KittyNavigateDown<cr>",  desc = "Move to panel Downd" },
-    { "<C-k>", "<cmd>KittyNavigateUp<cr>",    desc = "Move to panel Up" },
-    { "<C-l>", "<cmd>KittyNavigateRight<cr>", desc = "Move to panel Right" },
-  },
-  build = {
-    "mkdir -p ~/.config/kitty/kitty_navigator",
-    "cp ./*.py ~/.config/kitty/kitty_navigator",
-  },
-  config = function()
-    vim.g.kitty_navigator_no_mappings = 1
-  end,
+	"knubie/vim-kitty-navigator",
+	keys = {
+		{ "<C-h>", "<cmd>KittyNavigateLeft<cr>", mode = { "n", "t" }, desc = "Move to panel Left" },
+		{ "<C-j>", "<cmd>KittyNavigateDown<cr>", mode = { "n", "t" }, desc = "Move to panel Downd" },
+		{ "<C-k>", "<cmd>KittyNavigateUp<cr>", mode = { "n", "t" }, desc = "Move to panel Up" },
+		{ "<C-l>", "<cmd>KittyNavigateRight<cr>", mode = { "n", "t" }, desc = "Move to panel Right" },
+	},
+	build = {
+		"mkdir -p ~/.config/kitty/kitty_navigator",
+		"cp ./*.py ~/.config/kitty/kitty_navigator",
+	},
+	config = function()
+		vim.g.kitty_navigator_no_mappings = 1
+	end,
 }
