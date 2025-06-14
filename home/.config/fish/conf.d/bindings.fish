63e2306bdb725579f4ad9d1610997249fb94ce65
Vladimir Galović <vgalovic99@gmail.com>
2025-06-14 20:05:06 +0200

    Improve keybindings for fish shell

---
+++ home/.config/fish/conf.d/bindings.fish
@@ -0,0 +1,21 @@
+bind -e \eh
+bind -e \ej
+bind -e \ek
+bind -e \el
+bind -e \ed
+
+bind -e \ch
+bind -e \cj
+bind -e \ck
+bind -e \cl
+bind -e \cd
+
+bind \em '__fish_man_page'
+
+bind \e/ '__fish_list_current_token'
+
+bind \cd kill-line
+
+bind \er clear-screen
+
+bind \cq delete-or-exit
