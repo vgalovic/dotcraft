d853a64796e2bd70b300fa78b82cde7ce6256bc3
Vladimir Galović <vgalovic99@gmail.com>
2025-11-11 22:49:02 +0100

    refactor(nvim): replace `root_dirs` with `root_markers` in verible.lua

--- home/.config/nvim/lsp/verible.lua
+++ home/.config/nvim/lsp/verible.lua
@@ -1,6 +1,6 @@
 return {
 	cmd = { "verible-verilog-ls", "--rules_config_search" },
 	filetypes = { "verilog", "systemverilog" },
-	root_dir = vim.fs.find({ ".git", ".rules.verible_lint" }, { upward = true })[1] or vim.fn.getcwd(),
+	root_markers = { ".git", ".rules.verible_lint" },
 	single_file_support = true,
 }
