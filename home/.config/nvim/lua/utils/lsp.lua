64a9dac52e0d0b4349ffcc129b4fe182f3cc91d9
Vladimir Galović <vgalovic99@gmail.com>
2025-11-11 11:07:34 +0100

    feat(nvim): Combaine files in dashbord/ and lsp/ so it needs less `require` in config

--- home/.config/nvim/lua/utils/lsp.lua
+++ home/.config/nvim/lua/utils/lsp.lua
@@ -3,6 +3,10 @@
 
 local M = {}
 
+--========================================================================
+--                          Autocommands
+--========================================================================
+
 -- Utility functions shared between progress reports for LSP and DAP
 function M.setup_lsp_autocommands()
 	local augroup_lsp_attach = vim.api.nvim_create_augroup("lsp-attach", {})
@@ -11,8 +15,7 @@ function M.setup_lsp_autocommands()
 	vim.api.nvim_create_autocmd("LspAttach", {
 		group = augroup_lsp_attach,
 		callback = function(event)
-			local lsp_keymap = require("lsp.keymaps")
-			lsp_keymap.setup_lsp_keymaps(event.buf)
+			M.setup_lsp_keymaps(event.buf)
 
 			local client = vim.lsp.get_client_by_id(event.data.client_id)
 			if client and client.server_capabilities.documentHighlightProvider then
@@ -88,6 +91,34 @@ function M.setup_lsp_progress()
 			})
 		end,
 	})
+end
+
+--========================================================================
+--                              Keymaps
+--========================================================================
+
+function M.setup_lsp_keymaps(bufnr)
+	local map = vim.keymap.set
+	local snacks = require("snacks")
+
+	local function lsp_map(keys, func, desc, mode)
+		mode = mode or "n"
+		map(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+	end
+
+    -- Use `snacks` for LSP navigation
+    -- stylua: ignore start
+    lsp_map("gd", function() snacks.picker.lsp_definitions() end, "Goto Definition")
+    lsp_map("gy", function() snacks.picker.lsp_type_definitions() end, "Type Definition")
+    lsp_map("gR", function() snacks.picker.lsp_references() end, "Goto References")
+    lsp_map("gI", function() snacks.picker.lsp_implementations() end, "Goto Implementation")
+    lsp_map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+    lsp_map("<leader>cs", function() snacks.picker.lsp_symbols() end, "Document Symbols")
+    lsp_map("<leader>cw", function() snacks.picker.lsp_symbols({ cwd = vim.fn.expand("%:p:h") }) end, "Workspace Symbols")
+    lsp_map("<leader>cc", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
+    lsp_map("K", vim.lsp.buf.hover, "Buffer hover")
+    lsp_map("<backspace>", vim.lsp.buf.rename, "Rename")
+	-- stylua: ignore end
 end
 
 return M
