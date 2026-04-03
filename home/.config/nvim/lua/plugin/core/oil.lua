294aed272b7c45c51999c80844bc1390a1e627b2
Vladimir Galović <vgalovic99@gmail.com>
2026-04-03 11:45:28 +0200

    refactor(neovim): changed map in oil.nvim from `~` to `g~`

--- home/.config/nvim/lua/plugin/core/oil.lua
+++ home/.config/nvim/lua/plugin/core/oil.lua
@@ -30,7 +30,7 @@ return {
 			["<backspace>"] = { "actions.parent", mode = "n" },
 			["_"] = { "actions.open_cwd", mode = "n" },
 			["`"] = { "actions.cd", mode = "n" },
-			["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+			["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
 			["gs"] = { "actions.change_sort", mode = "n" },
 			["gx"] = "actions.open_external",
 			["g."] = { "actions.toggle_hidden", mode = "n" },
