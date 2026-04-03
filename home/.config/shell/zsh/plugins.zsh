0c0fb3ccf60a47a0db0cf8137fe349cff1c634fb
Vladimir Galović <vgalovic99@gmail.com>
2026-04-03 14:29:57 +0200

    refactor(shell): moved eza aliases to shared alsias.sh

--- home/.config/shell/zsh/plugins.zsh
+++ home/.config/shell/zsh/plugins.zsh
@@ -11,7 +11,7 @@ export KEYTIMEOUT=1
 # Asynchronous plugin initialization
 async_start_worker plugin_async
 
-async_job plugin_load() {
+async_jobplugin_load() {
   # Starship - Prompt theme
   eval "$(starship init zsh)"
 
@@ -22,11 +22,6 @@ async_job plugin_load() {
   alias cat="bat"
   alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
   alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
-
-  # Eza - Enhanced `ls` replacement
-  alias ls="eza --color=always --icons=always --hyperlink"
-  alias la="eza --color=always --icons=always --hyperlink --all"
-  alias ll="eza --color=always --long --git --icons=always --hyperlink --all"
 }
 
 # Asynchronous job to load plugins
