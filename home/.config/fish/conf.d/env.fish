97e4853728839e7a265cd1dc9f309ce3d9f87a35
Vladimir Galović <vgalovic99@gmail.com>
2026-04-03 11:12:40 +0200

    fix: reinitialized git repo after manual directory transfer

--- home/.config/fish/conf.d/env.fish
+++ home/.config/fish/conf.d/env.fish
@@ -1,6 +1,15 @@
-# Set SYSTEMC path
+# SYSTEMC
 set -gx SYSTEMC /usr/local/systemc
 set -gx LD_LIBRARY_PATH /usr/local/systemc/lib $LD_LIBRARY_PATH
+
+# Cargo
+set -gx PATH $HOME/.cargo/bin $PATH
+
+# User local bin
+set -gx PATH $HOME/.local/bin $PATH
+
+# NPM global
+set -gx PATH $HOME/.npm-global/bin $PATH
 
 # Editor and pager
 set -gx EDITOR (which nvim)
